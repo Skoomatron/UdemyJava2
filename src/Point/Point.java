0c40afb0fddb5d60e0720c2d387cf74f1798e2c1
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-20 13:09:45 -0400

    Point Complete

---
+++ src/Point/Point.java
@@ -0,0 +1,43 @@
+package Point;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance() {
+        return Math.sqrt((0 - this.x) * (0 - this.x) + (0 - this.y) * (0 - this.y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+}
