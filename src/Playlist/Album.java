713b1c9a7ce1a6ea76c18ebb27c5aa3ce714f19d
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-24 09:14:05 -0400

    Playlist Refactored for Inner Class

--- src/Playlist/Album.java
+++ src/Playlist/Album.java
@@ -7,42 +7,61 @@ public class Album {
 
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
     public boolean addSong(String title, double duration) {
-        if (findSong(title) == null) {
-            this.songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
-    }
-    public Song findSong(String title) {
-        for (Song searched: this.songs) {
-            if (searched.getTitle().equals(title)) {
-                return searched;
-            }
-        }
-        return null;
+        return this.songs.add(new Song(title, duration));
     }
     public boolean addToPlayList(int track, LinkedList<Song> playlist) {
-        int index = track -1;
-        if (index >= 0 && index <= this.songs.size()) {
-            playlist.add(this.songs.get(index));
+        Song check = this.songs.findSong(track);
+        if (check != null) {
+            playlist.add(check);
             return true;
         }
         return false;
     }
     public boolean addToPlayList(String title, LinkedList<Song> playlist) {
-        Song searched = findSong(title);
-        if (searched != null) {
-            playlist.add(searched);
+        Song check = songs.findSong(title);
+        if (check != null) {
+            playlist.add(check);
             return true;
         }
         return false;
+    }
+    public static class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+        private boolean add(Song song) {
+            if (songs.contains(song)) {
+                return false;
+            }
+            this.songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song checked: this.songs) {
+                if (checked.getTitle().equals(title)) {
+                    return checked;
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int track) {
+            int index = track - 1;
+            if (index >= 0 && index <= this.songs.size()) {
+                return songs.get(index);
+            }
+            return null;
+        }
     }
 }
